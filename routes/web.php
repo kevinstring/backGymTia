2d488fbc75a6ff6b320ff4c8df7a1d5a41d80436
kevinstring <105533147+kevinstring@users.noreply.github.com>
2025-01-22 18:10:20 -0600

    get jeje

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::post('guardarCliente','App\Http\Controllers\gymController@guardarCliente'
 Route::get('getUsuariosInscritos','App\Http\Controllers\gymController@getUsuariosInscritos');
 Route::post('actualizarCliente','App\Http\Controllers\gymController@actualizarCliente');
 Route::post('/send-whatsapp-message', 'App\Http\Controllers\WhatsAppController@sendMessage');
+Route::get('getRegistros', 'App\Http\Controllers\gymController@getRegistros');
