43208a9262112231172a4e22e86b229f28f4b1a9
kevinstring <105533147+kevinstring@users.noreply.github.com>
2025-01-23 17:16:21 -0600

    noEntender

--- app/Console/Commands/mandarMensajes.php
+++ app/Console/Commands/mandarMensajes.php
@@ -2,12 +2,8 @@
 
 namespace App\Console\Commands;
 
-
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use App\Services\WhatsAppService;
-use Illuminate\Http\Request;
-
 
 
 class mandarMensajes extends Command
@@ -29,65 +25,20 @@ class mandarMensajes extends Command
     /**
      * Execute the console command.
      */
+    public function handle()
+    {
+        //
 
-     private $whatsappService;
+        $obtenerDias = DB::TABLE("USUARIO_INSCRITO")->get();
 
-     public function __construct(WhatsAppService $whatsappService)
-     {
-         $this->whatsappService = $whatsappService;
-     }
- 
-     public function sendMessage(Request $request)
-     {
-         // Obtener los datos del request
-         $to = $request->input('to'); // El número de teléfono
-         $templateName = $request->input('name'); // El nombre de la plantilla
-         $languageCode = $request->input('code', 'es'); // El código de idioma (por defecto "es")
-     
-         // Obtener los parámetros desde el request
-         $parameters = [
-             [
-                 'type' => 'text',
-                 'parameter_name' => 'name', // Nombre del parámetro en la plantilla
-                 'text' =>'Texto de prueba etiquetar a persona' // Valor del parámetro
-             ],
-             [
-                 'type' => 'text',
-                 'parameter_name' => 'dias', // Nombre del parámetro en la plantilla
-                 'text' =>'50' // Valor del parámetro
-             ]
-         ];
-     
-         // Validar que los parámetros estén bien formateados
-         if (!is_array($parameters) || empty($parameters)) {
-             return response()->json(['error' => 'El parámetro "parameters" debe ser un array y no estar vacío'], 400);
-         }
-     
-   
-     }
-    // public function handle()
-    // {
-    //     //
+        foreach($obtenerDias as $dias){
+            if($dias->DIAS_PENDIENTES==2){
+                
 
-    //     $obtenerDias = DB::TABLE("USUARIO_INSCRITO")->
-    //     leftjoin("USUARIO as user")
-    //     ->get();
+            }
+        }
 
 
-    //     foreach($obtenerDias as $dias){
-    //         if($dias->DIAS_PENDIENTES==2){
 
-
-    //             $response = $this->whatsappService->sendMessage('+50235518257', $templateName, 'es', $parameters);
-
-
-    //         }
-    //     }
-    //   // Llamar al servicio de WhatsApp para enviar el mensaje
-     
-    //   // Retornar la respuesta del servicio
-    //   return response()->json($response);
-
-
-    // }
+    }
 }
